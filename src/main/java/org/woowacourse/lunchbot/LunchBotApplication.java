64c606a359c6fb0314243cca8a492989ec931a94
jaeju.jang <jjj0611@gmail.com>
2020-02-17 16:02:16 +0900

    init. create lunch-bot repository to start to make a lunch bot in woowacourse

---
+++ src/main/java/org/woowacourse/lunchbot/LunchBotApplication.java
@@ -0,0 +1,13 @@
+package org.woowacourse.lunchbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunchBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/woowacourse/lunchbot/LunchBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.woowacourse.lunchbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
