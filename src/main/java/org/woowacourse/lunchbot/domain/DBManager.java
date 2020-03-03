431288ff6556135a83962983852da7891c3b2b4d
moonyoungCHAE <xpf_fl@naver.com>
2020-03-03 19:48:30 +0900

    refactor. package 생성 및 클래스 이동

--- src/main/java/org/woowacourse/lunchbot/domain/DBManager.java
+++ src/main/java/org/woowacourse/lunchbot/domain/DBManager.java
@@ -1,4 +1,4 @@
-package org.woowacourse.lunchbot.service;
+package org.woowacourse.lunchbot.domain;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
