1861fb0b1cb4379bd2145fe38ff575f79d0109cf
moonyoungCHAE <xpf_fl@naver.com>
2020-03-16 16:05:36 +0900

    refactor: 시간 확인 방식 수정

--- src/main/java/org/woowacourse/lunchbot/service/EatTogetherService.java
+++ src/main/java/org/woowacourse/lunchbot/service/EatTogetherService.java
@@ -6,22 +6,16 @@ import org.woowacourse.lunchbot.domain.UserProfile;
 import org.woowacourse.lunchbot.slack.dto.request.BlockActionRequest;
 
 import java.time.LocalTime;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.time.ZonedDateTime;
+import java.time.temporal.TemporalQuery;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @Service
 public class EatTogetherService {
 
-    private static final Map<String, List<LocalTime>> TIME_SCOPE = initializeTimeScope();
-    private static final String LUNCH_APPLY = "lunchApply";
-    private static final String DINNER_APPLY = "dinnerApply";
-    private static final String LUNCH_RESULT = "lunchResult";
-    private static final String DINNER_RESULT = "dinnerResult";
-    private static final int START_TIME_INDEX = 0;
-    private static final int END_TIME_INDEX = 1;
+    private static final TemporalQuery<Boolean> applyTime;
+    private static final TemporalQuery<Boolean> resultTime;
     private static final String SUCCESS_MESSAGE = "성공적으로 등록되었습니다.";
     private static final String ALREADY_APPLIED = "이미 신청한 사용자입니다";
     private static final String REQUEST_FAILURE_MESSAGE = "요청에 실패했습니다.";
@@ -31,26 +25,34 @@ public class EatTogetherService {
     private final SlackApiService slackApiService;
     private final UserMatcher userMatcher;
 
-    private static Map<String, List<LocalTime>> initializeTimeScope() {
-        Map<String, List<LocalTime>> timeScope = new HashMap<>();
-        List<LocalTime> lunchApply = Arrays.asList(LocalTime.of(9, 0), LocalTime.of(11, 20));
-        List<LocalTime> dinnerApply = Arrays.asList(LocalTime.of(16, 0), LocalTime.of(17, 30));
-        List<LocalTime> lunchResult = Arrays.asList(LocalTime.of(11, 30), LocalTime.of(13, 30));
-        List<LocalTime> dinnerResult = Arrays.asList(LocalTime.of(18, 0), LocalTime.of(20, 0));
-        timeScope.put(LUNCH_APPLY, lunchApply);
-        timeScope.put(DINNER_APPLY, dinnerApply);
-        timeScope.put(LUNCH_RESULT, lunchResult);
-        timeScope.put(DINNER_RESULT, dinnerResult);
-        return timeScope;
-    }
 
     public EatTogetherService(SlackApiService slackApiService, UserMatcher userMatcher) {
         this.slackApiService = slackApiService;
         this.userMatcher = userMatcher;
     }
 
+
+    static {
+        applyTime =
+                temporal -> {
+                    LocalTime now = LocalTime.now();
+                    return (now.compareTo(LocalTime.of(9, 0)) >= 0
+                            && now.compareTo(LocalTime.of(11, 20)) < 0)
+                            || (now.compareTo(LocalTime.of(16, 0)) >= 0
+                            && now.compareTo(LocalTime.of(17, 30)) < 0);
+                };
+
+        resultTime = temporal -> {
+            LocalTime now = LocalTime.now();
+            return (now.compareTo(LocalTime.of(11, 30)) >= 0
+                    && now.compareTo(LocalTime.of(13, 30)) < 0)
+                    || (now.compareTo(LocalTime.of(18, 0)) >= 0
+                    && now.compareTo(LocalTime.of(20, 0)) < 0);
+        };
+    }
+
     public String apply(BlockActionRequest request) {
-        if (!validateApplyTimeScope()) {
+        if (!applyTime.queryFrom(ZonedDateTime.now())) {
             return NOT_AVAILABLE_TIME;
         }
         Optional<UserProfile> userProfile = slackApiService.getUserProfile(request.getUserId());
@@ -61,7 +63,7 @@ public class EatTogetherService {
     }
 
     public List<List<UserProfile>> getMatchedUsers() {
-        if (!validateResultTimeScope()) {
+        if (!resultTime.queryFrom(ZonedDateTime.now())) {
             throw new IllegalArgumentException(NOT_AVAILABLE_TIME);
         }
         List<List<UserProfile>> matchedUsers = userMatcher.getMatchedUsers();
@@ -69,18 +71,5 @@ public class EatTogetherService {
             throw new IllegalArgumentException(NO_ONE_APPLIED);
         }
         return userMatcher.getMatchedUsers();
-    }
-
-    private boolean validateApplyTimeScope() {
-        return isBetween(TIME_SCOPE.get(LUNCH_APPLY)) || isBetween(TIME_SCOPE.get(DINNER_APPLY));
-    }
-
-    private boolean validateResultTimeScope() {
-        return isBetween(TIME_SCOPE.get(LUNCH_RESULT)) || isBetween(TIME_SCOPE.get(DINNER_RESULT));
-    }
-
-    private boolean isBetween(List<LocalTime> times) {
-        LocalTime now = LocalTime.now();
-        return now.isAfter(times.get(START_TIME_INDEX)) && now.isBefore(times.get(END_TIME_INDEX));
     }
 }
